47fbfd7db0609fdacbd3c196c26c85244f2554af
mennamb26-droid <menna.mb26@gmail.com>
2026-07-04 14:17:03 +0300

    Change base path in Vite config to '/menna_work/'

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-vue/',
+  base: '/menna_work/',
   plugins: [
     vue(),
     vueDevTools(),
